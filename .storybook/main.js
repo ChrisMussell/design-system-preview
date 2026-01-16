6cb2441302b80d79256787ca71540fb175efdd85
ChrisMussell <mussell.chris@gmail.com>
2026-01-16 08:50:38 +0000

    Update main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,21 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
-  framework: {
-    name: '@storybook/react-vite',
-    options: {},
-  },
-  docs: {
-    autodocs: 'tag',
-  },
-};
-export default config;
+1/** @type { import('@storybook/react-vite').StorybookConfig } */
+2const config = {
+3  stories: [
+4    '../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+5    '../src/stories/**/*.mdx'
+6  ],
+7  addons: [
+8    '@storybook/addon-links',
+9    '@storybook/addon-essentials',
+10    '@storybook/addon-interactions',
+11  ],
+12  framework: {
+13    name: '@storybook/react-vite',
+14    options: {},
+15  },
+16  docs: {
+17    autodocs: 'tag',
+18  },
+19};
+20export default config;
+21
